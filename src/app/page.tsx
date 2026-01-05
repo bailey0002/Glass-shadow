3c719683a38a23ced8964f137f094ce7a9095664
bailey0002 <mark.baileyjr@outlook.com>
2026-01-05 11:10:24 -0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import GlassShadowGame from '@/components/GlassShadowGame'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-[#0a0c0e]">
+      <GlassShadowGame />
+    </main>
+  )
+}
